d3459f4860e3a578295d299fdf187fb20ec9275f
nguyengiabao245 <101686610+nguyengiabao245@users.noreply.github.com>
2023-03-31 09:59:27 +0700

    Add files via upload

--- shopping_cart.php
+++ shopping_cart.php
@@ -33,3 +33,53 @@
         header("Location: shoppong_cart.php");
     }
 ?>
+
+<div class="container text-center">
+    <div class="col-sm-3">
+        <h3>Danh mục</h3>
+        <ul class="list-group">
+            <?php
+                foreach($cates as $item) {
+                    echo "<li class ='list-group-item'><a href=/list_product.php?cateid=".$item["CateID"].">".$item["CategoryName"]."</a></li>";
+                }
+                ?>
+        </ul>
+    </div>
+    <div class="col-sm-9">
+<h3>Thông tin giỏ hàng</h3><br>
+<table class="table table-condensed">
+    <thead>
+        <tr>
+            <th>Tên sản phẩm</th>
+            <th>Hình ảnh</th>
+            <th>Số lượng</th>
+            <th>Đơn giá</th>
+            <th>Thành tiền</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            $total_money = 0;
+            if(isset($_SESSION["cart_items"]) && count($_SESSION['cart_items'])>0){
+                foreach($_SESSION["cart_items"] as $item){
+                    $id = $item["pro_id"];
+                    $product = Product::get_product($id);
+                    $prod = reset($product);
+                    $total_money += $item["quantity"]*$prod["Price"];
+                    echo "<tr><td>" .$prod["ProductName"]."</td><td><img style='width:90px; height:80px' src=".$prod["Picture"]."/>
+                    </td><td>".$item["quantity"]."</td> <td>".$prod["Price"]."</td><td>".$prod["Price"]."</td></tr>";
+                }
+
+                echo "<tr> <td colspan=5><p class='text-right text-danger'>Tổng tiền".$total_money."</p></td></tr>";
+                echo "<tr> <td colspan=3><p class='text-right'><button type='button' class='btn btn-primary'>Tiếp tục mua hàng</button></p></td><td colspan=2><p class='text-right'><button type='button' class'btn btn-success'>Thanh toán</button></td></tr>";
+            }
+            else{
+                echo "không có sản phẩn nào trong giỏ hàng";
+            }
+            ?>
+    </tbody>
+</table>
+    </div>
+</div>
+
+<?php include_once("footer.php")?>
